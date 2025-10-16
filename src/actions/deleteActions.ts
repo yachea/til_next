da1fdd4b1acedb0fe2080322f31667d3c11a0c6e
yachea <dev.yachea@gmail.com>
2025-10-16 12:29:56 +0900

    [docs] Server Action의 이해

--- src/actions/deleteActions.ts
+++ src/actions/deleteActions.ts
@@ -24,7 +24,7 @@ export async function deleteAction(_: any, formData: FormData) {
     console.log(error);
     return {
       status: false,
-      messsage: `해당하는 ${id}가 삭제에 실패했습니다. 다시 시도해주세요.`,
+      messsage: `해당하는 ${goodId}가 삭제에 실패했습니다. 다시 시도해주세요.`,
     };
   }
 }
